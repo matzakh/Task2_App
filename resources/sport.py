95923953e0cf12e71b9c924f279c147dba5c71d5
Matvey Zakharuk <matvey.zakharuk@socialtechnologies.io>
2022-09-15 16:37:25 +0100

    moved parameter validation to a schema

--- resources/sport.py
+++ resources/sport.py
@@ -1,18 +1,25 @@
 from flask_restful import fields, marshal_with, reqparse, Resource
 from models.sport_model import SportModel
 from common.utils import abort_if_not_exist
+from marshmallow import Schema, fields
+
+
+class SportSchema(Schema):
+    name = fields.Str(required=False)
+    slug = fields.Str(required=False)
+    active = fields.Boolean(required=False)
 
 
 class Sport(Resource):
-    post_parser = reqparse.RequestParser()
+    """ post_parser = reqparse.RequestParser()
     post_parser.add_argument(
         'name', dest='name',
-        location='form', required=True
+        location='form', required=False
     )
     post_parser.add_argument(
         'slug', dest='slug',
         location='form',
-        required=True
+        required=False
     )
     post_parser.add_argument(
         'active', dest='active',
@@ -24,7 +31,7 @@ class Sport(Resource):
         'name': fields.String,
         'slug': fields.String,
         'active': fields.Boolean
-    }
+    } """
 
     def get(self, slug):
         result = SportModel.find_by_slug(slug)
@@ -32,11 +39,11 @@ class Sport(Resource):
             abort_if_not_exist(slug)
         return result.json()
 
-    @marshal_with(sport_fields)
+    # @marshal_with(sport_fields)
     def post(self, **kwargs):
         pass
 
-    @marshal_with(sport_fields)
+    # @marshal_with(sport_fields)
     def put(self, **kwargs):
         pass
 
@@ -46,5 +53,5 @@ class Sport(Resource):
 
 class SportList(Resource):
 
-    def get(self):
+    def get(self, **kwargs):
         pass
