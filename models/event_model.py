da32fabf20138126cc05aa673dd693ad7e71feb7
Matvey Zakharuk <matvey.zakharuk@socialtechnologies.io>
2022-09-18 14:30:35 +0100

    added event_model

---
+++ models/event_model.py
@@ -0,0 +1,42 @@
+from db import db
+from flask import jsonify
+from collections import namedtuple
+from enum import Enum
+
+
+class EventType(Enum):
+    PREPAY = 1
+    INPLAY = 2
+
+
+class EventStatus(Enum):
+    PENDING = 1
+    STARTED = 2
+    ENDED = 3
+    CANCELLED = 4
+
+
+class EventModel(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    slug = db.Column(db.String(50), nullable=False)
+    active = db.Column(db.Boolean)
+    type = db.Column(db.Integer)
+    sport = db.Column(db.Integer, db.ForeignKey('sport_model.id'))
+    scheduled_start = db.Column(db.DATETIME)
+    actual_start = db.Column(db.DATETIME)
+
+    def __repr__(self):
+        return "<Event %r>" % self.name
+
+    def json(self):
+        return jsonify({
+            'name': self.name,
+            'slug': self.slug,
+            'active': self.active,
+            'type': self.type,
+            'sport': self.sport,
+            'scheduled_start': self.scheduled_start,
+            'actual_start': self.actual_start
+        })
