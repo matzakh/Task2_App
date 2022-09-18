c052c22e0c5241f6c44a012d1f409619f10080f3
Matvey Zakharuk <matvey.zakharuk@socialtechnologies.io>
2022-09-18 15:48:42 +0100

    added event resource

--- resources/event.py
+++ resources/event.py
@@ -1,2 +1,13 @@
-class Event:
-    pass
+from flask_restful import request, Resource, abort
+from models.event_model import EventModel
+from common.utils import abort_if_not_exist
+from marshmallow import Schema, fields
+
+
+class Event(Resource):
+
+    def get(self, slug):
+        result = EventModel.find_by_field(slug)
+        if result is None:
+            abort_if_not_exist(slug)
+        return result.json()
