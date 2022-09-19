958da197b05a6d1726737ee2f97137e22e224d81
Matvey Zakharuk <matvey.zakharuk@socialtechnologies.io>
2022-09-19 16:42:06 +0100

    added parser for OR

--- common/utils.py
+++ common/utils.py
@@ -8,8 +8,12 @@ def abort_if_not_exist(item):
     abort(404, message="{} doesn't exist".format(str(item)))
 
 
-def parse_OR_clauses_for_query(key, value):
-
+def parse_clauses_for_query(key, operator, value):
+    if '|' in value:
+        variants = value.split('|')
+        return '(' + ' OR '.join([key + ' ' + operator + ' ' + v for v in variants]) + ')'
+    else:
+        return '(' + key + ' ' + operator + ' ' + value + ')'
 
 
 def populate_db(sqldb):
