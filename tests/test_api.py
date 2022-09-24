6b3f3a377634d36f9e9fcc66c3126886a8365945
Matvey Zakharuk <matvey.zakharuk@socialtechnologies.io>
2022-09-24 17:52:18 +0100

    added check for active boolean update

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,9 +1,10 @@
 import unittest
+import json
 
 from app import app
 
 
-APP_URL = 'localhost:5000'
+APP_URL = 'http://127.0.0.1:5000'
 
 
 class TestApi(unittest.TestCase):
@@ -12,6 +13,11 @@ class TestApi(unittest.TestCase):
         self.app = app
         self.app.config['TESTING'] = True
         self.client = self.app.test_client()
+
+    def test_app_working(self):
+        with self.app.app_context():
+            response = self.client.get(APP_URL + '/')
+            self.assertEqual(response.status_code, 200)
 
     def test_sport_not_exist(self):
         with self.app.app_context():
@@ -27,6 +33,29 @@ class TestApi(unittest.TestCase):
         with self.app.app_context():
             response = self.client.get(APP_URL + '/selection/madeup')
             self.assertEqual(response.status_code, 404)
+
+    def test_update_active(self):
+        with self.app.app_context():
+            response = self.client.put(APP_URL + '/selection/4', data={'active': [1]})
+            response_event = self.client.get(APP_URL + '/event/search?id=3&active=0')
+            response_sport = self.client.get(APP_URL + '/sport/search?id=2&active=0')
+            response1 = response.status_code
+            event_is_active_response1 = response_event.status_code
+            sport_is_active_response1 = response_sport.status_code
+
+            response = self.client.put(APP_URL + '/selection/4', data={'active': [0]})
+            response_event = self.client.get(APP_URL + '/event/search?id=3&active=1')
+            response_sport = self.client.get(APP_URL + '/sport/search?id=2&active=1')
+            response2 = response.status_code
+            event_is_active_response2 = response_event.status_code
+            sport_is_active_response2 = response_sport.status_code
+
+            self.assertEqual(response1, 200)
+            self.assertEqual(response2, 200)
+            self.assertEqual(event_is_active_response1, 404)
+            self.assertEqual(event_is_active_response2, 404)
+            self.assertEqual(sport_is_active_response1, 404)
+            self.assertEqual(sport_is_active_response2, 404)
 
 
 if __name__ == "__main__":
