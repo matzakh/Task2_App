4aa996d2e3a5c4d9c63f956e5f3c65be3c066bd4
Matvey Zakharuk <matvey.zakharuk@socialtechnologies.io>
2022-09-24 17:10:42 +0100

    added basic tests for 404

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,10 +1,32 @@
 import unittest
 
-from .. import app
+from app import app
+
+
+APP_URL = 'localhost:5000'
 
 
 class TestApi(unittest.TestCase):
-    pass
+
+    def setUp(self):
+        self.app = app
+        self.app.config['TESTING'] = True
+        self.client = self.app.test_client()
+
+    def test_sport_not_exist(self):
+        with self.app.app_context():
+            response = self.client.get(APP_URL + '/sport/madeup')
+            self.assertEqual(response.status_code, 404)
+
+    def test_event_not_exist(self):
+        with self.app.app_context():
+            response = self.client.get(APP_URL + '/event/madeup')
+            self.assertEqual(response.status_code, 404)
+
+    def test_selection_not_exist(self):
+        with self.app.app_context():
+            response = self.client.get(APP_URL + '/selection/madeup')
+            self.assertEqual(response.status_code, 404)
 
 
 if __name__ == "__main__":
