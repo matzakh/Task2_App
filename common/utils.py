7ab28194dfc27c0966804e642e3b3d927d3f959f
Matvey Zakharuk <matvey.zakharuk@socialtechnologies.io>
2022-09-25 18:07:14 +0100

    added timezone converter

--- common/utils.py
+++ common/utils.py
@@ -1,4 +1,7 @@
+import pytz
+
 from flask_restful import abort
+from datetime import datetime as dt
 
 
 def abort_if_not_exist(item):
@@ -22,17 +25,23 @@ def parse_clauses_for_query(key, operator, value, is_string=False):
 def parse_key_val_with_operator(keystr):
     if '>=' in keystr:
         return keystr.split('>=')[0], '>=', keystr.split('>=')[1]
-    elif '<=' in keystr:
+    if '<=' in keystr:
         return keystr.split('<=')[0], '<=', keystr.split('<=')[1]
-    elif '>' in keystr:
+    if '>' in keystr:
         return keystr.split('>')[0], '>', keystr.split('>')[1]
-    elif '<' in keystr:
+    if '<' in keystr:
         return keystr.split('<')[0], '<', keystr.split('<')[1]
-    else:
-        return None
+    return None
 
 
 def if_none_replace_with_strnull(val):
     if val is None:
         return 'NULL'
     return val
+
+
+def strdate_timezone_to_utc_convert(str_dt, timezone, pattern="%Y-%m-%d %H:%M:%S"):
+    input_date = dt.strptime(str_dt, pattern).replace(tzinfo=pytz.timezone(timezone))
+    output_date = input_date.astimezone(pytz.utc)
+    return output_date.strftime(pattern)
+
